c92cd13557d47d40b1a228c4ee6ae0e0e3ab9b04
David Weis <dweis7@gmail.com>
2021-09-30 18:08:00 +0100

    Make rotating example more interesting

--- pose-publisher/examples/publisher_rotation.rs
+++ pose-publisher/examples/publisher_rotation.rs
@@ -14,12 +14,31 @@ fn main() -> Result<(), PosePublisherError> {
 
     let pose_publisher = PosePublisher::new(args.address)?;
     loop {
-        std::thread::sleep(std::time::Duration::from_secs_f32(0.02));
-        let mut update = PoseClientUpdate::new();
-        update
-            .add("rotated_object", (0., 0., 0.1))
-            .with_shape(Shape::Cube(0.3, 0.01, 0.01))
-            .with_rotation((0.9848, 0., 0.1736, 0.));
-        pose_publisher.publish(update)?;
+        for i in 0..=100 {
+            let i = i as f32 / 100.0;
+            let w = 1.0 - i;
+            let y = 0.0 + i;
+
+            std::thread::sleep(std::time::Duration::from_secs_f32(0.02));
+            let mut update = PoseClientUpdate::new();
+            update
+                .add("rotated_object", (0., 0., i))
+                .with_shape(Shape::Cube(0.3, 0.01, 0.01))
+                .with_rotation((w, 0., y, 0.));
+            pose_publisher.publish(update)?;
+        }
+        for i in (0..=100).rev() {
+            let i = i as f32 / 100.0;
+            let w = 1.0 - i;
+            let y = 0.0 + i;
+
+            std::thread::sleep(std::time::Duration::from_secs_f32(0.02));
+            let mut update = PoseClientUpdate::new();
+            update
+                .add("rotated_object", (0., 0., i))
+                .with_shape(Shape::Cube(0.3, 0.01, 0.01))
+                .with_rotation((w, 0., y, 0.));
+            pose_publisher.publish(update)?;
+        }
     }
 }
