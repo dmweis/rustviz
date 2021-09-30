5b95e9187b58ceb7f1771b9162331da8f5ecbf23
dmweis <dmweis@users.noreply.github.com>
2021-09-30 15:03:08 +0000

    deploy: 64c6706b6bb5fbf1d367bbaea163e409f6e8cdc9

--- source-files.js
+++ source-files.js
@@ -1,7 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["example_point_cloud_publisher"] = {"name":"","files":["example_point_cloud_publisher.rs"]};
-sourcesIndex["example_publisher"] = {"name":"","files":["example_publisher.rs"]};
-sourcesIndex["example_subscriber"] = {"name":"","files":["example_subscriber.rs"]};
 sourcesIndex["pose_publisher"] = {"name":"","files":["lib.rs","multicast.rs","point_cloud.rs","pose.rs"]};
 sourcesIndex["rustviz_ui"] = {"name":"","files":["main.rs"]};
 createSourceSidebar();
